7d002a5801717fa757bf200596b2db5d2cd7d7b4
Bhagya <bhagyanadukandy@gmail.com>
2019-08-19 12:01:51 +0530

    structured the json file

--- group.py
+++ group.py
@@ -2,7 +2,7 @@ import json
 from pdf_to_txt import pdf_to_text1
 from stemm import stemming
 
-f_name ='Linkedin/Daniel Loman.pdf'
+f_name = 'Linkedin/onePageResume.pdf'
 # give the name of json file to be created
 def grouping():
     json_file = 'txt_with_header.json'
@@ -19,30 +19,30 @@ def grouping():
     incase personal details not found then take first 10 lines a personal details then proceed
     some times experience and skills are mixed up 
     """
-
+    final_json =[]
     with open(json_file, 'w') as fp:
-            for line in result:
-                data = dict()
-                #print(line)
-                sentence = line
-                if (sentence.strip("\n").strip() and sentence!="\u00a0\n" and sentence!="\f\u00a0\n" and sentence!="\f" and sentence!="\f"):
-                    if len(sentence.split()) < 4:
-                        #print("sentence less than 4====",sentence)
-                        for ele in header_keywords:
-                            #print("these are the elements",ele,) 
-                            #print("sdntence in lower",sentence.lower(),"fdaf")
-                            sentence_lower = sentence.lower().strip()
-                            stemmed_sentence = stemming(sentence_lower)
-                            #print("type of ele",type(ele))
-                            #print("type of check",type(sentence_lower))
-                            if any(ele in s for s in stemmed_sentence):
-                                print("match found****************************************")
-                                headermqin = ele
-                            
-                                
+        for line in result:
+            data = dict()
+            #print(line)
+            sentence = line
+            if (sentence.strip("\n").strip() and sentence!="\u00a0\n" and sentence!="\f\u00a0\n" and sentence!="\f" and sentence!="\f"):
+                if len(sentence.split()) < 4:
+                    #print("sentence less than 4====",sentence)
+                    for ele in header_keywords:
+                        #print("these are the elements",ele,) 
+                        #print("sdntence in lower",sentence.lower(),"fdaf")
+                        sentence_lower = sentence.lower().strip()
+                        stemmed_sentence = stemming(sentence_lower)
+                        #print("type of ele",type(ele))
+                        #print("type of check",type(sentence_lower))
+                        if any(ele in s for s in stemmed_sentence):
+                            print("match found****************************************")
+                            headermqin = ele
                         
-                    data['text'] = sentence
-                    data['header'] = headermqin
-                    line_new = json.dumps(data) + '\n'
-                    fp.write(line_new)
+                            
+                    
+                data['text'] = sentence
+                data['header'] = headermqin
+                final_json.append(data)
+        fp.write(json.dumps(final_json))
 grouping()
